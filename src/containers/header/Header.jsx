e508c09133fa2c4eae1751c1a936b60e638f9b00
Syhh885403 <hieuho285@gmail.com>
2023-02-17 23:05:00 -0600

    update

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,8 +1,33 @@
-import React from "react"
-import "./header.css"
+import React from "react";
+import "./header.css";
+import peopleImg from "../../assets/people.png";
+import ai from "../../assets/ai.png";
 
 function Header() {
-    return <div>Header</div>
+	return (
+		<div className="header">
+			<div className="header__content">
+				<h1>Let’s Build Something amazing with GPT-3 OpenAI</h1>
+				<p>
+					Yet bed any for travelling assistance indulgence unpleasing. Not
+					thoughts all exercise blessing. Indulgence way everything joy
+					alteration boisterous the attachment. Party we years to order allow
+					asked of.
+				</p>
+				<div className="header__input">
+					<input placeholder="Your email adress" />
+					<button>Get Started</button>
+				</div>
+				<div className="header__people">
+					<img src={peopleImg} />
+					<p>1,600 people requested access a visit in last 24 hours</p>
+				</div>
+			</div>
+			<div className="header__image">
+				<img src={ai} />
+			</div>
+		</div>
+	);
 }
 
-export default Header
+export default Header;
--- src/containers/navbar/Navbar.jsx
+++ src/containers/navbar/Navbar.jsx
@@ -1,38 +1,65 @@
-import React from "react"
-import "./navbar.css"
-import logo from "../../assets/logo.svg"
-
+import React, { useState } from "react";
+import "./navbar.css";
+import logo from "../../assets/logo.svg";
+import { RiCloseLine, RiMenu3Line } from "react-icons/ri";
+import { SlLogin } from "react-icons/sl";
+import { SiGnuprivacyguard } from "react-icons/si";
 function Navbar() {
-    return (
-        <div className="navbar">
-            <div className="navbar__container">
-                <div className="navbar__logo">
-                    <img src={logo} />
-                </div>
-                <ul className="navbar__ul">
-                    <a href="#">
-                        <li>Home</li>
-                    </a>
-                    <a href="#">
-                        <li>What is GPT?</li>
-                    </a>
-                    <a href="#">
-                        <li>Open AI</li>
-                    </a>
-                    <a href="#">
-                        <li>Case Studies</li>
-                    </a>
-                    <a href="#">
-                        <li>Library</li>
-                    </a>
-                </ul>
-            </div>
-            <div className="navbar__sign">
-                <button>Sign in</button>
-                <button>Sign up</button>
-            </div>
-        </div>
-    )
+	const [toggleMenu, setToggleMenu] = useState(false);
+
+	const menu = (
+		<>
+			<a href="#">Home</a>
+			<a href="#">What is GPT?</a>
+			<a href="#">Open AI</a>
+			<a href="#">Case Studies</a>
+			<a href="#">Library</a>
+		</>
+	);
+
+	return (
+		<div className="navbar">
+			{toggleMenu && <div className="overlay"></div>}
+
+			<div className="navbar__container">
+				<div className="navbar__container_logo">
+					<img src={logo} />
+				</div>
+				<div className="navbar__container_nav">{menu}</div>
+			</div>
+			<div className="navbar_container_sign-icon">
+				<div>
+					<SlLogin className="navbar_container_sign-icon_icon" />
+				</div>
+				<div>
+					<SiGnuprivacyguard className="navbar_container_sign-icon_icon" />
+				</div>
+				
+			</div>
+			<div className="navbar__container_sign">
+				<button>Sign in</button>
+				<button className="navbar__container_sign-up">Sign up</button>
+			</div>
+			<div className="navbar__mobile">
+				{toggleMenu ? (
+					<RiCloseLine
+						className="navbar__mobile_icon"
+						color="#fff"
+						size={27}
+						onClick={() => setToggleMenu((toggle) => !toggle)}
+					/>
+				) : (
+					<RiMenu3Line
+						className="navbar__mobile_icon"
+						color="#fff"
+						size={27}
+						onClick={() => setToggleMenu((toggle) => !toggle)}
+					/>
+				)}
+				{toggleMenu && <div className="navbar__mobile_nav">{menu}</div>}
+			</div>
+		</div>
+	);
 }
 
-export default Navbar
+export default Navbar;
